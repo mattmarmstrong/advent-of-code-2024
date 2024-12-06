069fb8e55283d0913678ecba4685991f4ec20274
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2024-12-06 11:17:25 -0600

    weirdly proud of this one (I shouldn't be)

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -1,3 +1,105 @@
+#[derive(Debug)]
+enum Direction {
+    Right,
+    Down,
+    DiagonalDownLeft,
+    DiagonalDownRight,
+}
+
+const XMAS: [&str; 2] = ["XMAS", "SAMX"];
+const X_MAS: [&str; 2] = ["MAS", "SAM"];
+
+fn can_move_dir(dir: &Direction, x_pos: usize, y_pos: usize, max_x: usize, max_y: usize) -> bool {
+    match dir {
+        Direction::Right => (x_pos + 3) <= max_x,
+        Direction::Down => (y_pos + 3) <= max_y,
+        Direction::DiagonalDownLeft => x_pos >= 3 && (y_pos + 3 <= max_y),
+        Direction::DiagonalDownRight => (x_pos + 3 <= max_x) && (y_pos + 3 <= max_y),
+    }
+}
+
+fn can_form_x(
+    x_pos: usize,
+    y_pos: usize,
+    max_x: usize,
+    max_y: usize,
+    puzzle: &[Vec<char>],
+) -> bool {
+    puzzle[y_pos][x_pos] == 'A'
+        && (y_pos >= 1)
+        && (x_pos >= 1)
+        && (y_pos < max_y)
+        && (x_pos < max_x)
+}
+
+fn scan_in_dir(dir: &Direction, x_pos: usize, y_pos: usize, puzzle: &[Vec<char>]) -> String {
+    let mut ret = String::with_capacity(4);
+    for n in 0..=3 {
+        match dir {
+            Direction::Right => ret.push(puzzle[y_pos][x_pos + n]),
+            Direction::Down => ret.push(puzzle[y_pos + n][x_pos]),
+            Direction::DiagonalDownRight => ret.push(puzzle[y_pos + n][x_pos + n]),
+            Direction::DiagonalDownLeft => ret.push(puzzle[y_pos + n][x_pos - n]),
+        }
+    }
+    ret
+}
+
+fn is_x_mas(x_pos: isize, y_pos: isize, puzzle: &[Vec<char>]) -> bool {
+    let mut arm_1 = String::with_capacity(3);
+    let mut arm_2 = String::with_capacity(3);
+    for n in (-1)..=1 {
+        arm_1.push(puzzle[(y_pos + n) as usize][(x_pos + n) as usize]);
+        arm_2.push(puzzle[(y_pos + n) as usize][(x_pos - n) as usize]);
+    }
+    X_MAS.contains(&arm_1.as_str()) && X_MAS.contains(&arm_2.as_str())
+}
+
+fn parse(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|ln| ln.chars().collect()).collect()
+}
+
+pub fn solve_pt1(input: &str) -> usize {
+    let puzzle = parse(input);
+    let max_x = puzzle[0].len() - 1;
+    let max_y = puzzle.len() - 1;
+    let mut acc = 0;
+    for j in 0..=max_y {
+        for i in 0..=max_x {
+            for dir in [
+                Direction::Down,
+                Direction::Right,
+                Direction::DiagonalDownLeft,
+                Direction::DiagonalDownRight,
+            ] {
+                if can_move_dir(&dir, i, j, max_x, max_y) {
+                    let scan = scan_in_dir(&dir, i, j, &puzzle);
+                    if XMAS.contains(&scan.as_str()) {
+                        acc += 1
+                    }
+                }
+            }
+        }
+    }
+    acc
+}
+
+pub fn solve_pt2(input: &str) -> usize {
+    let puzzle = parse(input);
+    let max_x = puzzle[0].len() - 1;
+    let max_y = puzzle.len() - 1;
+    let mut acc = 0;
+    for j in 0..=max_y {
+        for i in 0..=max_x {
+            if can_form_x(i, j, max_x, max_y, &puzzle) && is_x_mas(i as isize, j as isize, &puzzle)
+            {
+                acc += 1
+            }
+        }
+    }
+    acc
+}
+
 #[cfg(test)]
 
 mod tests {
@@ -13,13 +115,13 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
 
-    // #[test]
-    // fn test_pt1() {
-    //     assert_eq!(solve_pt1(EXAMPLE), 11)
-    // }
+    #[test]
+    fn test_pt1() {
+        assert_eq!(solve_pt1(EXAMPLE), 18)
+    }
 
-    // #[test]
-    // fn test_pt2() {
-    //     assert_eq!(solve_pt2(EXAMPLE), 31)
-    // }
+    #[test]
+    fn test_pt2() {
+        assert_eq!(solve_pt2(EXAMPLE), 9)
+    }
 }
---
+++ src/day_5/mod.rs
@@ -0,0 +1,175 @@
+use hashbrown::{HashMap, HashSet};
+use itertools::Itertools;
+
+const DOUBLE_NEWLINE: &str = "\n\n";
+const SEPARATOR: &str = "|";
+const COMMA: &str = ",";
+
+type OrderMap = HashMap<usize, HashSet<usize>>;
+type Updates = Vec<Vec<usize>>;
+
+fn parse(input: &str) -> (OrderMap, Updates) {
+    let (order, updates): (&str, &str) = input.split(DOUBLE_NEWLINE).collect_tuple().unwrap();
+    let mut order_map: OrderMap = HashMap::new();
+    order.lines().for_each(|ln| {
+        let (first, second): (usize, usize) = ln
+            .split(SEPARATOR)
+            .map(|x| x.parse::<usize>().ok().unwrap())
+            .collect_tuple()
+            .unwrap();
+        if order_map.contains_key(&first) {
+            let v = order_map.get_mut(&first).unwrap();
+            v.insert(second);
+        } else {
+            let mut v = HashSet::new();
+            v.insert(second);
+            order_map.insert(first, v);
+        }
+    });
+    let updates = updates
+        .lines()
+        .map(|ln| {
+            ln.split(COMMA)
+                .take_while(|x| !x.is_empty())
+                .map(|x| x.parse::<usize>().ok().unwrap())
+                .collect()
+        })
+        .collect();
+    (order_map, updates)
+}
+
+fn check_if_ord(order_map: &OrderMap, update_ord: &Vec<usize>) -> bool {
+    let l = update_ord.len();
+    if update_ord.is_empty() || l == 1 {
+        return true;
+    }
+    update_ord.iter().enumerate().all(|(i, pg)| {
+        if i < l {
+            update_ord[0..i].iter().all(|x| {
+                if order_map.contains_key(pg) {
+                    let befores = order_map.get(pg).unwrap();
+                    !befores.contains(x)
+                } else {
+                    true
+                }
+            })
+        } else {
+            true
+        }
+    })
+}
+
+#[inline]
+fn get_median(v: &[usize]) -> usize {
+    let m = ((v.len() / 2) as f64).ceil() as usize;
+    v[m]
+}
+
+fn get_next(order_map: &OrderMap, un_ord: &Vec<usize>) -> Vec<usize> {
+    let first = *un_ord.first().unwrap();
+    let mut tmp = Vec::with_capacity(un_ord.len());
+    un_ord.iter().for_each(|n| {
+        if order_map.contains_key(n) {
+            let befores = order_map.get(n).unwrap();
+            if befores.contains(&first) {
+                tmp.push(*n);
+            }
+        }
+    });
+    if tmp.is_empty() {
+        vec![first]
+    } else if check_if_ord(order_map, &tmp) {
+        tmp
+    } else {
+        get_next(order_map, &tmp)
+    }
+}
+
+fn order(order_map: &OrderMap, un_ord: &mut Vec<usize>) -> Vec<usize> {
+    let mut tmp = Vec::with_capacity(un_ord.len());
+    while !un_ord.is_empty() {
+        let ord = get_next(order_map, un_ord);
+        tmp.append(&mut ord.clone());
+        let new_vec = un_ord
+            .iter()
+            .filter(|x| !(ord.contains(x)))
+            .copied()
+            .collect::<Vec<usize>>();
+        let _ = std::mem::replace(un_ord, new_vec);
+        if check_if_ord(order_map, un_ord) {
+            tmp.append(un_ord);
+            break;
+        }
+    }
+    tmp
+}
+
+pub fn solve_pt1(input: &str) -> usize {
+    let (ord_map, updates) = parse(input);
+    updates
+        .iter()
+        .map(|u| {
+            if check_if_ord(&ord_map, u) {
+                get_median(u)
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+pub fn solve_pt2(input: &str) -> usize {
+    let (ord_map, mut updates) = parse(input);
+    updates
+        .iter_mut()
+        .filter(|u| !check_if_ord(&ord_map, u))
+        .map(|u| {
+            let m = get_median(order(&ord_map, u).as_slice());
+            m
+        })
+        .sum()
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    const EXAMPLE: &str = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+
+    #[test]
+    fn test_pt1() {
+        assert_eq!(solve_pt1(EXAMPLE), 143)
+    }
+
+    #[test]
+    fn test_pt2() {
+        assert_eq!(solve_pt2(EXAMPLE), 123)
+    }
+}
